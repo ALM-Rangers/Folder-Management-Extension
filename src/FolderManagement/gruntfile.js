53888651aca8d6fb2cb0f9f04baae8d45a1a350f
Wouter de Kort <wouter.dekort@seizeit.nl>
2016-03-29 16:57:34 +0200

    Get script from node_modules and place in scripts/libs

--- src/FolderManagement/gruntfile.js
+++ src/FolderManagement/gruntfile.js
@@ -7,23 +7,13 @@
                 stdout: true,
                 stderr: true
             },
-            update: {
-                command: "npm up --save-dev",
+            publish: {
+                command: "tfx extension publish --token <token> --manifest-globs vss-extension.json",
                 stdout: true,
                 stderr: true
             },
             initdev: {
                 command: "npm install && typings install ",
-                stdout: true,
-                stderr: true
-            },
-            tsdlink: {
-                command: "tsd link",
-                stdout: true,
-                stderr: true
-            },
-            publish: {
-                command: "tfx extension publish --token ejie2d22jvcrarhqvmrpnjnzyuusd4lb53w4dmxdzdkiwcykrfna --manifest-globs vss-extension-me.json",
                 stdout: true,
                 stderr: true
             }
@@ -36,33 +26,11 @@
                       flatten: true,
                       src: [
                           'node_modules/vss-sdk/lib/VSS.SDK.js',
-                          'node_modules/moment/moment.js',
-                          'node_modules/jquery/dist/jquery.js',
-                          'node_modules/spectrum-colorpicker/spectrum.js',
-                          'node_modules/spectrum-colorpicker/spectrum.css'
+                          'node_modules/jquery/dist/jquery.min.js'
                       ],
                       dest: 'scripts/lib',
                       filter: 'isFile'
-                  },
-                  {
-                      expand: true,
-                      flatten: true,
-                      src: [
-                          'node_modules/moment/moment.js',
-                      ],
-                      dest: './',
-                      filter: 'isFile'
-                  },
-                {
-                    expand: true,
-                    flatten: true,
-                    src: [
-                        'node_modules/moment/moment.js',
-                    ],
-                    dest: 'tests/',
-                    filter: 'isFile'
-                }
-                ]
+                  }]
             }
         },
         typescript: {
@@ -70,7 +38,7 @@
                 src: ['scripts/**/*.ts'],
                 dest: 'scripts',
                 options: {
-                    module: 'amd', //or commonjs 
+                    module: 'amd', 
                     target: 'es5',
                     sourceMap: true,
                     declaration: true
